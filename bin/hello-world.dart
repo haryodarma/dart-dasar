9180b27c123d979182f688b1f125c79ce5cb3669
haryodarma <haryo.darmap@gmail.com>
2024-12-07 12:51:00 +0800

    init: first initialization

---
+++ bin/hello-world.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("Hello Programming Community UBG");
+}
---
+++ bin/kata-kunci.dart
@@ -0,0 +1,17 @@
+void main() {
+// Kata Kunci Var
+  var community1 = "Programming Community";
+
+// Kata Kunci Dynamic
+  dynamic community2 = "Programming Community";
+
+// Kata Kunci Final
+  final community3 = "Programming Community";
+
+// Kata Kunci Const
+  const community4 = "Programming Community";
+
+// Kata Kunci Late
+  late var community5;
+  community5 = "Programmin Community";
+}
